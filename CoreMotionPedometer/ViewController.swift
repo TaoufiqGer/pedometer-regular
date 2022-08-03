8e5a6de4173f1fb319c06f0cf60b0d21a04d461b
Taoufiq Germoud <gt@oufiq.fr>
2022-08-03 20:50:45 +0200

    initial commit

--- CoreMotionPedometer/ViewController.swift
+++ CoreMotionPedometer/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distance: UILabel!
+    
+    var pedometer = CMPedometer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if CMPedometer.isDistanceAvailable() {
+            pedometer.startUpdates(from: Date()) { pedometerData, error in
+                if let pedometerData = pedometerData {
+                    self.distance.text = pedometerData.distance?.stringValue
+                }
+            }
+            
+        } else {
+            print("distance is not available")
+        }
+        
     }
 
 
